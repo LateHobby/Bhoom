1619084de307d2498d77e63b5c83ab5f38b0258b
LateHobby <shiva.chaudhuri@gmail.com>
2014-02-09 12:16:18 -0800

    Print engine identification. Experimental (disabled) code for incremental move generation and testing.

--- Bhoom/src/sc/engine/engines/CTestEngine.java
+++ Bhoom/src/sc/engine/engines/CTestEngine.java
@@ -4,13 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import sc.bboard.PositionInfo;
+import sc.encodings.Encodings;
 import sc.engine.EngineBoard;
+import sc.engine.EngineListener;
 import sc.engine.EvalTT;
 import sc.engine.Evaluator;
 import sc.engine.MoveSorter;
-import sc.util.DeepEquals;
-import sc.util.ObjectCloner;
-import sc.util.StringDump;
+import sc.engine.engines.AbstractEngine.LocalVars;
 
 public class CTestEngine extends AbstractEngine {
 
@@ -23,6 +23,8 @@ public class CTestEngine extends AbstractEngine {
 	private  boolean useHistoryHeuristic= true;
 	private  boolean useKillerMoves= true;
 	private  boolean useFutilityPruning= true;
+	
+	
 	
 	public CTestEngine(String name, SearchMode mode, int aspWin, Evaluator eval, EvalTT ttable,
 			MoveSorter sorter) {
@@ -44,8 +46,150 @@ public class CTestEngine extends AbstractEngine {
 		this.useHistoryHeuristic = useHistoryHeuristic;
 		this.useKillerMoves = useKillerMoves;
 		this.useFutilityPruning = useFutilityPruning;
+		System.out.println(getFlagString());
 	}
 
+//	@Override
+//	protected int alphaBeta(EngineBoard board, int alpha, int beta,
+//			int depthLeft, int ply, LocalVars localVars) {
+//		if (abandonSearch) {
+//			traceAbandonSearch();
+//			return beta; // Doesn't matter what is returned, so return cutoff
+//		}
+//		boolean quiesce = (depthLeft <= 0);
+//		stats.addNode(quiesce);
+//		localVars.alpha = alpha;
+//		
+//		if (retrieveEvalCutoff(board, alpha, beta, depthLeft, localVars)) {
+//			stats.ttHit(quiesce);
+//			return localVars.eval;
+//		}
+//		
+//		int staticEval = evaluator.evaluate(board);
+//		traceStaticEval(staticEval);
+//		if (isDraw(board)) {
+//			return -contemptFactor(board, staticEval);
+//		}
+//		
+//		alpha = localVars.alpha;
+//		
+//		if (ply > 0 && noMoveEvalCutoff(board, alpha, beta, staticEval, depthLeft, ply, quiesce, localVars)) {
+////			store(board, alpha, beta, localVars.eval, 0, depthLeft);
+//			stats.betaCutoff(0, quiesce);
+//			return localVars.eval;
+//		}
+//		alpha = localVars.alpha;
+//
+//		
+//		
+//		if (isGeneralFutilityPruned(board, alpha, beta, staticEval, depthLeft)) {
+//			localVars.eval = alpha;
+//			return alpha;
+//		}
+//		int score;
+//		int bestScore = Evaluator.MIN_EVAL;
+//		int bestMove = 0;
+//		boolean pvFound = false;
+//		int newDepthLeft = (depthLeft <= 0) ? 0 : depthLeft - 1;
+//		
+//		
+//		
+//		if (!localVars.moveHandler.generateMoves(board,  quiesce, localVars.hashMove, ply, historyHeuristicArray, killerMoves)) {
+//			return terminalEval(board, quiesce, staticEval, depthLeft);
+//		}
+//
+//		int i = 0;
+//		while (localVars.moveHandler.hasMoreMoves(ply, historyHeuristicArray, killerMoves)) {
+//			int move = localVars.moveHandler.nextMove();
+//			if (isSpecificFutilityPruned(board, move, alpha, beta, staticEval, depthLeft)) {
+//				bestScore = Math.max(bestScore, alpha);
+//				break;
+//			}
+//			makeMove(board, move);
+//			LocalVars childLocalVars = localVarsPool.allocate();
+//			if (pvFound && useLateMoveReduction() &&
+//					lateMoveReductionEval(board, alpha, beta, newDepthLeft, ply,
+//							quiesce, move, childLocalVars)) {
+//				score = childLocalVars.eval;
+//			} else {
+//				traceEnteredNode(board, alpha, beta, newDepthLeft, ply+1, move, EngineListener.NORMAL);
+//				score = -alphaBeta(board, -beta, -alpha, newDepthLeft, ply+1, 
+//						 childLocalVars);
+//				traceExitNode(score);
+////				localVars.currentPV.copy(lv.bestNodePV);
+//			}
+//			undoLastMove(board, move);
+//			
+//			if (score > bestScore) {
+//				bestScore = score;
+//				bestMove = move;
+//				localVars.bestChildPV.copy(childLocalVars.bestNodePV);
+//			} else {
+//				incrementHistoryHeuristicArray(move, false);
+//			}
+//			localVarsPool.release(childLocalVars);
+//			
+//			if (score >= beta) {
+//				stats.betaCutoff(i, quiesce);
+//				incrementHistoryHeuristicArray(move, true);
+//				addToKillerMoves(board, ply, move, localVars.hashMove);
+//				break;
+//			}
+//			if (score > alpha) {
+//				stats.alphaImprovement(i);
+//				alpha = score;
+//			}
+//			pvFound = true;
+//			i++;
+//		}
+//		boolean standPatIsBest = quiesce && staticEval > bestScore && !board.kingInCheck(board.getWhiteToMove());
+//		if (!standPatIsBest) {
+//			store(board, alpha, beta, bestScore, bestMove, depthLeft);
+//			localVars.bestNodePV.addMove(bestMove);
+//			localVars.bestNodePV.addChild(localVars.bestChildPV);
+//			localVars.bestMove = bestMove;
+//		}
+//		return standPatIsBest ? staticEval : bestScore;
+//
+//	}
+//
+//	protected int[][] historyHeuristicArray = new int[64][64];
+//	protected int[][] killerMoves = new int[200][4];
+//	
+//	@Override
+//	public void incrementHistoryHeuristicArray(int move, boolean increment) {
+//		short from = Encodings.getFromSquare(move);
+//		short to = Encodings.getToSquare(move);
+//		if (increment) {
+//			historyHeuristicArray[from][to]++;
+//		} else {
+//			if (historyHeuristicArray[from][to] > 0) {
+//				historyHeuristicArray[from][to]--;
+//			}
+//		}
+//	}
+//
+//	@Override
+//	public void addToKillerMoves(EngineBoard board, int ply, int move, int hashMove) {
+//		if (move == hashMove || isCapture(board, move)) { // don't add captures or the hash move
+//			return;
+//		}
+//		for (int i = killerMoves[ply].length - 2; i >= 0; i--) {
+//		    killerMoves[ply][i + 1] = killerMoves[ply][i];
+//		}
+//		killerMoves[ply][0] = move;
+//	}
+//	
+//	protected boolean isCapture(EngineBoard board, int move) {
+//		if (Encodings.isEnpassantCapture(move)) {
+//			return true;
+//		} else {
+//			return board.getPiece(Encodings.getToSquare(move)) != Encodings.EMPTY;
+//		}
+//	}
+//
+	
+	
 	@Override
 	public boolean useTTable() {
 		return useTTable;
@@ -81,9 +225,6 @@ public class CTestEngine extends AbstractEngine {
 		return useFutilityPruning;
 	}
 
-	Map<Long, Integer> evalTestMap = new HashMap<Long, Integer>();
-	Map<Long, PositionInfo> posInfoMap = new HashMap<Long, PositionInfo>();
-	
 	private int minimaxNodeCount;
 	protected Continuation minimaxSearch(EngineBoard board, int depth, int dummy) {
 		LocalVars localVars = localVarsPool.allocate();
@@ -168,14 +309,14 @@ public class CTestEngine extends AbstractEngine {
 		
 	}
 	
-	private int simpleAlphaBeta(EngineBoard board, int alpha, int beta, int depthLeft,
+	protected int simpleAlphaBeta(EngineBoard board, int alpha, int beta, int depthLeft,
 			LocalVars localVars) {
 		minimaxNodeCount++;
 		if (minimaxNodeCount > 1 && minimaxNodeCount % 10000000 == 0) {
 			System.out.println("Alphabeta nodes:" + minimaxNodeCount);
 		}
-		int staticEval = evaluator.evaluate(board);
-//		storeStaticEval(board.getZobristKey(), staticEval, board.getPositionInfo());
+		int staticEval = getStaticEval(board);
+
 		traceStaticEval(staticEval);
 		if (isDraw(board)) {
 			return -contemptFactor(board, staticEval);
@@ -232,24 +373,10 @@ public class CTestEngine extends AbstractEngine {
 		return standPatIsBest? staticEval : bestScore;
 	}
 
-	private void storeStaticEval(long key, int staticEval, PositionInfo posInfo) {
-		
-		if (evalTestMap.containsKey(key)) {
-			if (staticEval != evalTestMap.get(key)) {
-				PositionInfo oldPosInfo = posInfoMap.get(key);
-				boolean posInfoEqual = DeepEquals.deepEquals(oldPosInfo, posInfo);
-				System.out.println(StringDump.dump(oldPosInfo));
-				System.out.println(StringDump.dump(posInfo));
-				throw new RuntimeException("Static eval mismatch: posInfoEqual=" + posInfoEqual);
-			}
-		}
-		try {
-			posInfoMap.put(key, (PositionInfo) ObjectCloner.deepCopy(posInfo));
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		evalTestMap.put(key, staticEval);
-		
+	// This method is overridden in subclasses for debugging
+	protected int getStaticEval(EngineBoard board) {
+		return evaluator.evaluate(board);
 	}
+
 
 }

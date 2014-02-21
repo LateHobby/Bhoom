5474949ac3d15ccf851df907d17289e16a54b7bd
LateHobby <shiva.chaudhuri@gmail.com>
2014-02-21 03:55:29 -0800

    Add some doc.

---
+++ Bhoom/misc/run_pandoc.bat
@@ -0,0 +1,3 @@
+pandoc -s -S --toc -c pandoc_john.css -c line_height_fix.css -o FutilityPruningNotes.html pandoc_source/FutilityPruningNotes.pandoc
+
+REM pandoc -s -S --toc -c pandoc.css -A footer.html README -o example3.html
